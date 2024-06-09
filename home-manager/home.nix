d5f5a7f64e82b9e92b1025d90854b527b3b16e30
grossamos <email@amosgross.com>
2024-06-09 13:21:35 +0200

    33 current  2024-06-09 13:21:28  24.11.20240605.e8057b6  6.6.32                          *

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -98,6 +98,16 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+
+    bash.enable = true; # see note on other shells below
+  };
+
   # ...
   dconf.settings = {
     # ...
