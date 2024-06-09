636531f68b593f4712d01b44ff15ece4f42d81aa
grossamos <email@amosgross.com>
2024-06-09 18:01:26 +0200

    35 current  2024-06-09 17:59:29  24.11.20240605.e8057b6  6.6.32                          *

--- system/configuration.nix
+++ system/configuration.nix
@@ -52,9 +52,9 @@
   programs.dconf.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable CUPS to print documents.
