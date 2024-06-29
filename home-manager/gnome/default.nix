7259cf4983b297f6f402ff0c4c3545c08ed56d40
grossamos <email@amosgross.com>
2024-06-30 00:38:02 +0200

    62 current  2024-06-30 00:37:57  24.11.20240627.b2852eb  6.6.36                          *

--- home-manager/gnome/default.nix
+++ home-manager/gnome/default.nix
@@ -25,6 +25,8 @@
       switch-to-application-7 = [];
       switch-to-application-8 = [];
       switch-to-application-9 = [];
+      open-new-window-application-1 = ["<Super><Shift><Enter>"];
+      open-new-window-application-2 = ["<Super><Enter>"];
     };
     "org/gnome/desktop/wm/keybindings" = {
       switch-to-workspace-left = ["<Super>Left"];
