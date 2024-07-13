a7a36f7ed6e0268a9131952b5ce12255e50c6d82
grossamos <email@amosgross.com>
2024-07-13 10:50:10 +0200

    88 current  2024-07-13 10:49:57  24.11.20240709.feb2849  6.6.37                          *

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -3,10 +3,7 @@
     enable = true;
     xwayland = {
       enable = true;
-      # hidpi = true;
     };
-    # enableNvidiaPatches = false;
-    systemd.enable = true;
   };
   home.packages = with pkgs; [
     rofi-wayland
@@ -16,30 +13,41 @@
     libnotify
     swww
   ];
-  systemd.user.targets.hyprland-session.Unit.Wants = ["xdg-desktop-autostart.target"];
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
-        "$mod + SHIFT, ENTER, exec, firefox"
-        "$mod, ENTER, exec, kitty"
-        ", Print, exec, grimblast copy area"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (
-            x: let
-              ws = let
-                c = (x + 1) / 10;
-              in
-                builtins.toString (x + 1 - (c * 10));
-            in [
-              "$mod, ${ws}, workspace, ${toString (x + 1)}"
-              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            ]
-          )
-          10)
-      );
+  wayland.windowManager.hyprland = {
+    settings = {
+      input = {
+        kb_layout = "us,fr";
+        kb_options = "grp:alt_caps_toggle";
+        numlock_by_default = true;
+        follow_mouse = 1;
+        sensitivity = 0;
+        touchpad = {
+          natural_scroll = true;
+        };
+      };
+      "$mod" = "SUPER";
+      bind =
+        [
+          "SUPER_SHIFT, ENTER, exec, firefox"
+          "SUPER, ENTER, exec, kitty"
+          ", Print, exec, grimblast copy area"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+          builtins.concatLists (builtins.genList (
+              x: let
+                ws = let
+                  c = (x + 1) / 10;
+                in
+                  builtins.toString (x + 1 - (c * 10));
+              in [
+                "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              ]
+            )
+            10)
+        );
+    };
   };
 }
