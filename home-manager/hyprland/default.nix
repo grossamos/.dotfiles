4b554e1826e5e6ae5d8f9eb613b6e2eff8014f1e
grossamos <email@amosgross.com>
2024-07-31 22:07:04 +0200

    99 current  2024-07-31 22:06:46  24.11.20240728.52ec9ac  6.6.43                          *

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -1,22 +1,24 @@
 {pkgs, ...}: {
+  home.packages = with pkgs; [
+    rofi-wayland # application launcher
+    hyprlock # lock screen
+    waybar # top bar
+    dunst # notification deamon
+    kitty
+    libnotify
+    swww # animated background
+    pipewire
+    wireplumber
+    polkit_gnome # for passwd popup when apps elevate privliges
+  ];
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland = {
       enable = true;
     };
-  };
-  home.packages = with pkgs; [
-    rofi-wayland
-    waybar
-    dunst
-    kitty
-    libnotify
-    swww
-  ];
-  wayland.windowManager.hyprland = {
     settings = {
       input = {
-        kb_layout = "us,fr";
+        kb_layout = "us,de";
         kb_options = "grp:alt_caps_toggle";
         numlock_by_default = true;
         follow_mouse = 1;
@@ -24,12 +26,27 @@
         touchpad = {
           natural_scroll = true;
         };
+        exec-once = [
+          "systemctl --user import-environment &"
+          "hash dbus-update-activation-environment 2>/dev/null &"
+          "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &"
+          "nm-applet &"
+          "wl-clip-persist --clipboard both"
+          "swaybg -m fill -i $(find ~/Pictures/wallpapers/ -maxdepth 1 -type f) &"
+          "hyprctl setcursor Nordzy-cursors 22 &"
+          "poweralertd &"
+          "waybar &"
+          "swaync &"
+          "wl-paste --watch cliphist store &"
+          "hyprlock"
+        ];
       };
+      monitor = "eDP-1,1920x1080@60,0x0,1";
       "$mod" = "SUPER";
       bind =
         [
-          "SUPER_SHIFT, ENTER, exec, firefox"
-          "SUPER, ENTER, exec, kitty"
+          "SUPER_SHIFT, return, exec, firefox"
+          "SUPER, return, exec, kitty"
           ", Print, exec, grimblast copy area"
         ]
         ++ (
