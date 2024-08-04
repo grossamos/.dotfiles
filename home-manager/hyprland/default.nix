642774c543973cfb22fd4931748a19f95f2f5fe6
grossamos <email@amosgross.com>
2024-08-04 15:47:26 +0200

    121 current  2024-08-04 15:47:17  24.11.20240802.d049530  6.6.43                          *

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -64,7 +64,7 @@
           "$mod SHIFT, R, exec, hyprctl reload"
           "$mod SHIFT, Q, killactive,"
           "$mod, Q, killactive,"
-          "$mod, R, exec, rofi -show combi -theme arthur,"
+          "$mod, R, exec, rofi -show combi -theme arthur"
           "$mod, F, fullscreen,"
           "$mod, Escape, exec, hyprlock"
           ", Print, exec, grimblast copy area"
