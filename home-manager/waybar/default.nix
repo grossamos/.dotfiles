d32b1d17766d80a8129a57b00c932811ada9edc2
grossamos <email@amosgross.com>
2024-08-04 22:37:22 +0200

    159 current  2024-08-04 22:37:04  24.11.20240802.d049530  6.6.43                          *

--- home-manager/waybar/default.nix
+++ home-manager/waybar/default.nix
@@ -3,6 +3,7 @@
     waybar
   ];
   programs.waybar = {
+    enable = true;
     settings.mainBar = {
       enable = true;
       position = "bottom";
@@ -12,6 +13,32 @@
       margin-bottom = 0;
       margin-left = 0;
       margin-right = 0;
+      modules-left = [
+        "custom/launcher"
+        "hyprland/workspaces"
+      ];
+      modules-center = [
+        "clock"
+      ];
+      modules-right = [
+        "pulseaudio"
+        "network"
+        "cpu"
+        "memory"
+        "battery"
+        "custom/notification"
+      ];
+      "hyprland/workspaces" = {
+        active-only = false;
+        disable-scroll = true;
+        format = "{icon}";
+        on-click = "activate";
+        format-icons = {
+          urgent = "";
+          default = "";
+          sort-by-number = true;
+        };
+      };
     };
   };
 }
