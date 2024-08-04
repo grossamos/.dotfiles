b978ffd8533951a4b67192eeba2be43d93b09a3b
grossamos <email@amosgross.com>
2024-08-04 21:14:45 +0200

    154 current  2024-08-04 21:14:31  24.11.20240802.d049530  6.6.43                          *

--- home-manager/browsers.nix
+++ home-manager/browsers.nix
@@ -1,20 +1,22 @@
 {inputs, ...}: {
-  home.file.".mozilla/firefox/amos/chrome/firefox-gnome-theme".source = inputs.firefox-gnome-theme;
+  # home.file.".mozilla/firefox/amos/chrome/firefox-gnome-theme".source = inputs.firefox-gnome-theme;
 
-  programs.firefox.profiles.amos = {
-    userChrome = ''
-      @import "firefox-gnome-theme/userChrome.css";
-    '';
-    userContent = ''
-      @import "firefox-gnome-theme/userContent.css";
-    '';
-    settings = {
-      ## Firefox gnome theme ## - https://github.com/rafaelmardojai/firefox-gnome-theme/blob/7cba78f5216403c4d2babb278ff9cc58bcb3ea66/configuration/user.js
-      # (copied into here because home-manager already writes to user.js)
-      "toolkit.legacyUserProfileCustomizations.stylesheets" = true; # Enable customChrome.cs
-      "browser.uidensity" = 0; # Set UI density to normal
-      "svg.context-properties.content.enabled" = true; # Enable SVG context-propertes
-      "browser.theme.dark-private-windows" = false; # Disable private window dark theme
-    };
-  };
+  # programs.firefox = {
+  #   enable = true;
+  #   profiles.default = {
+  #     name = "Default";
+  #     settings = {
+  #       "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+
+  #       # For Firefox GNOME theme:
+  #       "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+  #       "browser.tabs.drawInTitlebar" = true;
+  #       "svg.context-properties.content.enabled" = true;
+  #     };
+  #     userChrome = ''
+  #       @import "firefox-gnome-theme/userChrome.css";
+  #       @import "firefox-gnome-theme/theme/colors/dark.css";
+  #     '';
+  #   };
+  # };
 }
