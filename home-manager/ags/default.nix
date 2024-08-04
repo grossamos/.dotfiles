9e653a2a9129daa42025bb0efcf649b61c25da44
grossamos <email@amosgross.com>
2024-08-04 19:54:02 +0200

    145 current  2024-08-04 19:53:41  24.11.20240802.d049530  6.6.43                          *

--- home-manager/ags/default.nix
+++ home-manager/ags/default.nix
@@ -1,4 +1,5 @@
 {...}: {
   xdg.configFile."ags/config.js".source = ./config.js;
   xdg.configFile."ags/tsconfig.json".source = ./tsconfig.json;
+  xdg.configFile."ags/style.css".source = ./style.css;
 }
