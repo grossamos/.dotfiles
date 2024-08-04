5c3e3082dfbdde733414f9bf807c6d0dc9706dc2
grossamos <email@amosgross.com>
2024-08-04 20:25:19 +0200

    147 current  2024-08-04 20:25:03  24.11.20240802.d049530  6.6.43                          *

--- home-manager/ags/default.nix
+++ home-manager/ags/default.nix
@@ -1,15 +1,9 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  nixpkgs.overlays = [
-    (final: prev: {
-      ags = prev.ags.overrideAttrs (old: {
-        buildInputs = old.buildInputs ++ [pkgs.libdbusmenu-gtk3];
-      });
-    })
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    ags
+    libdbusmenu-gtk3
   ];
+
   xdg.configFile."ags/config.js".source = ./config.js;
   xdg.configFile."ags/tsconfig.json".source = ./tsconfig.json;
   xdg.configFile."ags/style.css".source = ./style.css;
