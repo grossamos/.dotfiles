e0178e3d525f50af3df912ab168dc0e9f6085f85
grossamos <email@amosgross.com>
2024-08-04 20:32:12 +0200

    149 current  2024-08-04 20:31:53  24.11.20240802.d049530  6.6.43                          *

--- home-manager/ags/default.nix
+++ home-manager/ags/default.nix
@@ -3,8 +3,4 @@
     ags
     libdbusmenu-gtk3
   ];
-
-  xdg.configFile."ags/config.js".source = ./config.js;
-  xdg.configFile."ags/tsconfig.json".source = ./tsconfig.json;
-  xdg.configFile."ags/style.css".source = ./style.css;
 }
