7831eafd93fcaa3693d902c8e335e82660de410c
grossamos <email@amosgross.com>
2024-08-04 22:50:27 +0200

    164 current  2024-08-04 22:50:09  24.11.20240802.d049530  6.6.43                          *

--- home-manager/waybar/default.nix
+++ home-manager/waybar/default.nix
@@ -34,26 +34,23 @@
         format = "{icon}";
         on-click = "activate";
         format-icons = {
-          "1" = "󰲠";
-          "2" = "󰲢";
-          "3" = "󰲤";
-          "4" = "󰲦";
-          "5" = "󰲨";
-          "6" = "󰲪";
-          "7" = "󰲬";
-          "8" = "󰲮";
-          "9" = "󰲰";
-          "10" = "󰿬";
           urgent = "";
+          active = "";
           default = "";
           empty = "";
         };
-        # format-icons = {
-        #   urgent = "";
-        #   default = "";
-        #   empty = "";
-        #   sort-by-number = true;
-        # };
+        persistent-workspaces = {
+          "1" = [];
+          "2" = [];
+          "3" = [];
+          "4" = [];
+          "5" = [];
+          "6" = [];
+          "7" = [];
+          "8" = [];
+          "9" = [];
+          "10" = [];
+        };
       };
     };
   };
