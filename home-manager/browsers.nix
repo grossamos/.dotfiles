0eca6084c7cab5926f8afa04a7ccc3c8a3abe774
grossamos <email@amosgross.com>
2024-08-04 21:19:53 +0200

    156 current  2024-08-04 21:17:51  24.11.20240802.d049530  6.6.43                          *

--- home-manager/browsers.nix
+++ home-manager/browsers.nix
@@ -1,22 +1,22 @@
 {inputs, ...}: {
-  # home.file.".mozilla/firefox/amos/chrome/firefox-gnome-theme".source = inputs.firefox-gnome-theme;
+  # home.file.".mozilla/firefox/default/chrome/firefox-gnome-theme".source = inputs.firefox-gnome-theme;
 
-  # programs.firefox = {
-  #   enable = true;
-  #   profiles.default = {
-  #     name = "Default";
-  #     settings = {
-  #       "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      name = "Default";
+      settings = {
+        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
 
-  #       # For Firefox GNOME theme:
-  #       "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-  #       "browser.tabs.drawInTitlebar" = true;
-  #       "svg.context-properties.content.enabled" = true;
-  #     };
-  #     userChrome = ''
-  #       @import "firefox-gnome-theme/userChrome.css";
-  #       @import "firefox-gnome-theme/theme/colors/dark.css";
-  #     '';
-  #   };
-  # };
+        # For Firefox GNOME theme:
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "browser.tabs.drawInTitlebar" = true;
+        "svg.context-properties.content.enabled" = true;
+      };
+      userChrome = ''
+        @import "firefox-gnome-theme/userChrome.css";
+        @import "firefox-gnome-theme/theme/colors/dark.css";
+      '';
+    };
+  };
 }
