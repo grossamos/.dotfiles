e2eab27945075461b3ad022894a9d9237933446c
grossamos <email@amosgross.com>
2024-08-05 21:58:21 +0200

    206 current  2024-08-05 21:57:59  24.11.20240802.d049530  6.6.43                          *

--- home-manager/waybar/style.nix
+++ home-manager/waybar/style.nix
@@ -3,7 +3,6 @@
     * {
         border-radius: 0;
         font-family: Roboto, Helvetica, Arial, sans-serif;
-        font-size: 18;
         color: white;
         min-height: 0;
     }
@@ -20,11 +19,12 @@
       padding: 2 5;
     }
 
-    .modules-middle {
+    .modules-center {
       margin-top: 10;
       margin-left: 10;
       background-color: #141c2f;
       border-radius: 20;
+      font-size: 18;
       padding: 2 5;
     }
   '';
