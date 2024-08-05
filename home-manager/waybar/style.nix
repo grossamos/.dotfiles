b11e8f02917413e58ebcb030c65f7f16e85b5307
grossamos <email@amosgross.com>
2024-08-05 22:12:50 +0200

    210 current  2024-08-05 22:12:29  24.11.20240802.d049530  6.6.43                          *

--- home-manager/waybar/style.nix
+++ home-manager/waybar/style.nix
@@ -1,10 +1,14 @@
 {...}: {
   xdg.configFile."waybar/style.css".text = ''
-    * {
-        border-radius: 0;
-        font-family: Roboto, Helvetica, Arial, sans-serif;
-        color: white;
-        min-height: 0;
+    .modules-center {
+      margin-top: 10;
+      margin-left: 10;
+      background-color: #141c2f;
+      border-radius: 20;
+      font-size: 18;
+      padding: 2 20;
+      font-weight: bold;
+      color: #e6818d;
     }
     window#waybar {
       padding-top: 5px;
@@ -19,15 +23,12 @@
       padding: 2 5;
     }
 
-    .modules-center {
-      margin-top: 10;
-      margin-left: 10;
-      background-color: #141c2f;
-      border-radius: 20;
-      font-size: 18;
-      padding: 2 20;
-      font-weight: bold;
-      color: #e6818d;
+    * {
+        border-radius: 0;
+        font-family: Roboto, Helvetica, Arial, sans-serif;
+        color: white;
+        min-height: 0;
     }
+
   '';
 }
