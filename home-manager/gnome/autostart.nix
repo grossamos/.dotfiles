27024c946e5f40d7479980fdd8e0294f2d432cc9
grossamos <email@amosgross.com>
2024-08-06 00:05:07 +0200

    235 current  2024-08-06 00:04:44  24.11.20240802.d049530  6.6.43                          *

--- home-manager/gnome/autostart.nix
+++ home-manager/gnome/autostart.nix
@@ -1,14 +1,21 @@
-{
-  xdg.configFile."autostart/syncthing.decktop".text = ''
-    [Desktop Entry]
-    Name=Syncthing Web UI
-    GenericName=File synchronization UI
-    Comment=Opens Syncthing's Web UI in the default browser (Syncthing must already be started).
-    Exec=/etc/profiles/per-user/amos/bin/syncthing serve --no-browser --logfile=default
-    Icon=syncthing
-    Terminal=false
-    Type=Application
-    Keywords=synchronization;interface;
-    Categories=Network;FileTransfer;P2P
-  '';
+{config, ...}: {
+  xdg.configFile."autostart/syncthing_init.decktop".conf = {
+    text = ''
+      [Desktop Entry]
+      Name=Syncthing Web UI
+      GenericName=File synchronization UI
+      Comment=Opens Syncthing's Web UI in the default browser (Syncthing must already be started).
+      Exec=/etc/profiles/per-user/amos/bin/syncthing serve --no-browser --logfile=default
+      Icon=syncthing
+      Terminal=false
+      Type=Application
+      Keywords=synchronization;interface;
+      Categories=Network;FileTransfer;P2P
+    '';
+    onChange = ''
+      rm -f ${config.xdg.configHome}/autostart/syncthing.desktop
+      cp ${config.xdg.configHome}/autostart/syncthing_init.desktop ${config.xdg.configHome}/autostart/syncthing.desktop
+      chmod 644 ${config.xdg.configHome}/autostart/syncthing.desktop
+    '';
+  };
 }
