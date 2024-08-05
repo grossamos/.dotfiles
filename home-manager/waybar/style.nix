36e54bcc9bf8dfaf3d02bf8dfc379eb9ce657447
grossamos <email@amosgross.com>
2024-08-05 21:10:27 +0200

    192 current  2024-08-05 21:10:05  24.11.20240802.d049530  6.6.43                          *

--- home-manager/waybar/style.nix
+++ home-manager/waybar/style.nix
@@ -1,5 +1,13 @@
 {...}: {
   xdg.configFile."waybar/style.css".text = ''
+    * {
+        border: none;
+        border-radius: 0;
+        font-family: Roboto, Helvetica, Arial, sans-serif;
+        font-size: 13px;
+        min-height: 0;
+        background-color: red;
+    }
     window#waybar {
       background-color: red;
       border-bottom: 3px solid rgba(100, 114, 125, 0.5);
