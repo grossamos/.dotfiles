4b1b05e9c1544fa3e459092dd5b3da39f14da3ba
grossamos <email@amosgross.com>
2024-12-01 16:04:16 +0100

    292 current  2024-12-01 16:04:13  25.05.20241128.970e93b  6.6.63                          *

--- system/configuration.nix
+++ system/configuration.nix
@@ -147,7 +147,9 @@
       source-han-sans
       source-han-sans-japanese
       source-han-serif-japanese
-      # nerdfonts
+      nerd-fonts.fira-code
+      nerd-fonts.inconsolata-lgc
+      nerd-fonts.jetbrains-mono
     ];
     fontconfig = {
       enable = true;
